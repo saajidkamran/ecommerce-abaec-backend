5e43988c794005c77f92606ed235549a00dd98e2
Saajid <saajid@gapstars.net>
2022-03-28 09:35:05 +0530

    all routes are protected and working

--- src/controllers/UpdateItem.js
+++ src/controllers/UpdateItem.js
@@ -34,7 +34,7 @@ const update = async (req, res, next) => {
       });
     }
   } else {
-    return res.status(404).send({
+    return res.status(401).send({
       error: "Access denaied"
     });
   }
