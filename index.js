062884c4e145291f77477414a32d7fa58254012b
Saajid <saajid@gapstars.net>
2022-04-05 01:03:10 +0530

    make it better

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const app = require("../backend/src/routes/approutes");
-const { port, server } = require("../backend/src/config/server");
+const app = require("./src/routes/approutes");
+// const { port, server } = require("../backend/src/config/server");
 //local-port access
 app.listen(process.env.PORT || 5002);
